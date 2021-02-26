90f47cfd0abecbfd576ee0af5cc59008da64323f
Davy Michel-Picard <davympicard@hotmail.com>
2021-02-26 14:03:14 -0500

    Ajout conteneur bouton + alignement des li

--- themes/theme-dmp/header.php
+++ themes/theme-dmp/header.php
@@ -53,10 +53,11 @@
 			<div>3</div>
 		</section>
 		<?php endif ?>
-		<button id="un">1</button>
-		<button id="deux">2</button>
-		<button id="trois">3</button>
-
+		<div class="conteneur-bouton">
+			<div id="un">1</div>
+			<div id="deux">2</div>
+			<div id="trois">3</div>
+		</div>
 
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><img src="https://s2.svgbox.net/hero-solid.svg?ic=menu&color=000000" width="32" height="32"></button>
