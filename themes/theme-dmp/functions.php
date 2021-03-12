9ea5fe4155aee1f0710d43e916f7a4347d7843e9
Davy Michel-Picard <davympicard@hotmail.com>
2021-03-12 16:17:58 -0500

    Ajout de couleur et ajustment des balises a

--- themes/theme-dmp/functions.php
+++ themes/theme-dmp/functions.php
@@ -196,12 +196,13 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 
 
 
-function extraire_cours($query){
-	if ($query->is_category('cours'))
-	{
-		$query->set('posts_per_page', -1);
-		$query->set('orderby', 'title');
-		$query->set('order', 'asc');
-	}
+function extraire_cours_front_page($query){
+	if( !is_admin() && $query->is_front_page() && $query->is_main_query() ){
+
+	$query->set( 'category_name', 'cours' );
+	$query->set('posts_per_page', -1 );
+	$query->set('orderby', 'title');
+	$query->set('order', 'asc');
 }
-add_action('pre_get_posts', 'extraire_cours');
+}
+add_action('pre_get_posts','extraire_cours_front_page');
